ca1a4b34c046e2a9812bde700c8e98b5f8f0e412
SAKSHAM-GOGIA <sakshamgogia041@gmail.com>
2023-02-20 19:55:27 +0530

    HomePage done with Linking

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
-
-// import Aboutus from './components/Aboutus/Aboutus.js';
 import Navbar from './components/Navbar/Navbar.js';
-// import Services from './components/services/services.js';
-// import OurFeatures from './components/ourfeatures/ourfeatures.js';
 import Footer from './components/footer/footer.js'
-// import Hero from './components/Hero/Hero.js';
-// import Loginpage from './components/login-page/loginpage.js';
 import {
   BrowserRouter,
   Routes,
